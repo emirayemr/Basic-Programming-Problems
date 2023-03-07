0d06fe07aa1ec55af13723a778a0e6551c8713af
Emir Ay <106332223+emirayemr@users.noreply.github.com>
2023-03-07 13:35:57 +0300

    Add files via upload

---
+++ draw_simulator.java
@@ -0,0 +1,35 @@
+// OUTLINE OF THE PROJECT
+/*
+	For the quarter finals in Champions League 8 teams are seeded.
+	Assume the teams to be matched are following:
+		Roma, Barcelona, Juventus, Bayern Munich, Sevilla, Real Madrid, Manchester City, Liverpool
+
+	The aim of the project is printing all possible combinations for the quarter final matches.
+		Note that Team1 v Team2 is equivalent to Team2 v Team1
+ */
+
+package draw_simulator;
+
+import java.util.Scanner;
+
+public class draw_simulator {
+
+	public static void main(String[] args) {
+		
+		String[] teams = {"Roma", "Barcelona", "Juventus", "Bayern Munich", "Sevilla", "Real Madrid", "Manchester City", "Liverpool"};
+		
+		for (int i=0; i<8; i++) {
+			
+			String team1 = teams[i];
+			int j = i+1;
+			
+			for (; j<8; j++) {
+				
+				String team2 = teams[j];
+				System.out.println(team1+" - "+team2);
+				
+			}
+			
+		}
+	}
+}
